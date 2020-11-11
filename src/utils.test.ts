0eb936f07f892462eaba833d41882c1d817d44e0
Minoru Okuyama <okuyama@analogic.jp>
2020-11-11 09:08:41 +0900

    test: add util test

---
+++ src/utils.test.ts
@@ -0,0 +1,58 @@
+import { NodeModel } from "./types";
+import { mutateTree, compareItems } from "./utils";
+
+test("Mutate tree items", () => {
+  const treeBefore: NodeModel[] = [
+    {
+      id: 1,
+      parent: 0,
+      droppable: true,
+      text: "a",
+    },
+    {
+      id: 2,
+      parent: 0,
+      droppable: true,
+      text: "b",
+    },
+  ];
+
+  const treeAfter: NodeModel[] = mutateTree(treeBefore, 2, 1);
+
+  treeBefore[1].text = "c";
+
+  expect(treeAfter).toEqual([
+    {
+      id: 1,
+      parent: 0,
+      droppable: true,
+      text: "a",
+    },
+    {
+      id: 2,
+      parent: 1,
+      droppable: true,
+      text: "b",
+    },
+  ]);
+});
+
+test("Compare tree item order", () => {
+  const nodeA: NodeModel = {
+    id: 1,
+    parent: 0,
+    droppable: true,
+    text: "a",
+  };
+
+  const nodeB: NodeModel = {
+    id: 2,
+    parent: 0,
+    droppable: true,
+    text: "b",
+  };
+
+  expect(compareItems(nodeA, nodeB)).toBe(-1);
+  expect(compareItems(nodeB, nodeA)).toBe(1);
+  expect(compareItems(nodeA, nodeA)).toBe(0);
+});
