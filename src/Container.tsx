88e577d8eec1acebd8cff8067970ce6544312404
Ben Heidemann <ben@heidemann.co.uk>
2021-04-10 01:16:10 +0100

    refactor: pass canDrop callback to useDropContainer

--- src/Container.tsx
+++ src/Container.tsx
@@ -25,7 +25,7 @@ export const Container: React.FC<Props> = (props) => {
   }
 
   const view = [...groups, ...templates];
-  const [isOver, drop] = useDropContainer(context.tree, context.onDrop);
+  const [isOver, drop] = useDropContainer(props.parentId, context.tree, context.onDrop, context.canDrop);
   const classes = context.classes;
 
   let className = "";
