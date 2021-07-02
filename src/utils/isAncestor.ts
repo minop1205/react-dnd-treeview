e8fd2fc7dd92c56f325faa94986e9aa23101a27f
minop1205 <okuyama@analogic.jp>
2021-07-02 10:11:16 +0900

    refactor: split utility into separate files

---
+++ src/utils/isAncestor.ts
@@ -0,0 +1,23 @@
+import { NodeModel } from "../types";
+
+export const isAncestor = (
+  tree: NodeModel[],
+  sourceId: NodeModel["id"],
+  targetId: NodeModel["id"]
+): boolean => {
+  if (targetId === 0) {
+    return false;
+  }
+
+  const targetNode = tree.find((node) => node.id === targetId);
+
+  if (targetNode === undefined) {
+    return false;
+  }
+
+  if (targetNode.parent === sourceId) {
+    return true;
+  }
+
+  return isAncestor(tree, sourceId, targetNode.parent);
+};
