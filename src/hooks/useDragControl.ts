ab19879f25817aa35ae4777478bc5d32a5c11ec6
minop1205 <okuyama@analogic.jp>
2021-07-09 09:33:23 +0900

    docs(#45): add comment about useDragControl hook

--- src/hooks/useDragControl.ts
+++ src/hooks/useDragControl.ts
@@ -1,6 +1,10 @@
 import { useEffect, useContext } from "react";
 import { DragControlContext } from "../Tree";
 
+/**
+ * This is a hook to allow text selection by mouse in the text input area in a node.
+ * Temporarily disables node dragging while the pointer is over the text input area.
+ */
 export const useDragControl = (ref: React.RefObject<HTMLElement>): void => {
   const dragControlContext = useContext(DragControlContext);
 
