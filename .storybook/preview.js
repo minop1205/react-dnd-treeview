62d6c6bbfa55648aa8efef5ef772dc80c7bbf899
Minoru Okuyama <okuyama@analogic.jp>
2022-01-30 00:30:30 +0900

    feat: add global decorator for storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,16 @@
+import { DndProvider } from "react-dnd";
+import { MultiBackend } from "dnd-multi-backend";
+import { HTML5toTouch } from "../src/HTML5toTouch";
+
 export const parameters = {
   layout: "fullscreen",
   controls: { expanded: true },
 };
+
+export const decorators = [
+  (Story) => (
+    <DndProvider backend={MultiBackend} options={HTML5toTouch}>
+      <Story />
+    </DndProvider>
+  ),
+];
