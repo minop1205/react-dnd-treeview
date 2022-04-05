69be217861465c0bd5ab79141bc47dcf2e514fa0
Minoru Okuyama <okuyama@analogic.jp>
2022-04-06 00:09:35 +0900

    test: add interactions to CustomNode story

--- src/stories/examples/CustomNode/CustomNode.stories.tsx
+++ src/stories/examples/CustomNode/CustomNode.stories.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { Meta } from "@storybook/react";
+import { expect } from "@storybook/jest";
+import { within, userEvent, fireEvent } from "@storybook/testing-library";
 import { pageFactory } from "~/stories/pageFactory";
 import * as argTypes from "~/stories/argTypes";
 import { Tree } from "~/Tree";
 import { TreeProps } from "~/types";
 import { FileProperties } from "~/stories/types";
+import {
+  dragEnterAndDragOver,
+  dragLeaveAndDragEnd,
+  dragAndDrop,
+  getPointerCoords,
+  assertElementCoords,
+} from "~/stories/examples/helpers";
 import { CustomNode } from "~/stories/examples/components/CustomNode";
+import { interactionsDisabled } from "~/stories/examples/interactionsDisabled";
 import { DefaultTemplate } from "~/stories/examples/DefaultTemplate";
 import sampleData from "~/stories/assets/sample-default.json";
 import styles from "./CustomNode.module.css";
@@ -41,3 +51,71 @@ CustomNodeStory.parameters = {
     }),
   },
 };
+
+if (!interactionsDisabled) {
+  CustomNodeStory.play = async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    // count nodes
+    expect(canvas.getAllByRole("listitem").length).toBe(3);
+
+    // open and close first node
+    expect(canvas.queryByText("File 1-1")).toBeNull();
+
+    userEvent.click(canvas.getByTestId("arrow-right-icon-1"));
+    expect(await canvas.findByText("File 1-1")).toBeInTheDocument();
+
+    userEvent.click(canvas.getByTestId("arrow-right-icon-1"));
+    expect(canvas.queryByText("File 1-1")).toBeNull();
+
+    // drag and drop: File 3 into Folder 1
+    await dragAndDrop(
+      canvas.getByText("File 3"),
+      canvas.getByTestId("custom-node-1")
+    );
+    expect(canvas.queryByText("File 3")).toBeNull();
+
+    // open Folder1
+    userEvent.click(canvas.getByTestId("arrow-right-icon-1"));
+    expect(await canvas.findByText("File 3")).toBeInTheDocument();
+
+    // drag and drop: File 3 into Folder 2
+    await dragAndDrop(
+      canvas.getByText("File 3"),
+      canvas.getByTestId("custom-node-4")
+    );
+    expect(canvas.queryByText("File 3")).toBeNull();
+
+    // open Folder2
+    userEvent.click(canvas.getByTestId("arrow-right-icon-4"));
+
+    // drag and drop: Folder 2 into Folder 1
+    await dragAndDrop(
+      canvas.getByText("Folder 2"),
+      canvas.getByTestId("custom-node-1")
+    );
+
+    assertElementCoords(await canvas.findByTestId("custom-node-4"), 32, 64);
+
+    // drag and drop: File 1-2 into root node
+    await dragAndDrop(
+      canvas.getByText("File 1-2"),
+      canvas.getAllByRole("list")[0]
+    );
+
+    assertElementCoords(await canvas.findByTestId("custom-node-3"), 32, 192);
+
+    // drag File3 and cancel drag
+    {
+      const dragSource = canvas.getByText("File 3");
+      const dropTarget = canvas.getAllByRole("list")[0];
+      const coords = getPointerCoords(dropTarget);
+
+      fireEvent.dragStart(dragSource);
+      await dragEnterAndDragOver(dropTarget, coords);
+      dragLeaveAndDragEnd(dragSource, dropTarget);
+
+      assertElementCoords(await canvas.findByTestId("custom-node-7"), 32, 128);
+    }
+  };
+}
