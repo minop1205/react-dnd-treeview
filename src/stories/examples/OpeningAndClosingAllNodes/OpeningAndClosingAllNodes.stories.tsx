41946b3070d15f3f3870846933108a216cbccd69
Minoru Okuyama <okuyama@analogic.jp>
2022-04-06 11:40:21 +0900

    test: add interactions to OpeningAndClosingAllNodes story

--- src/stories/examples/OpeningAndClosingAllNodes/OpeningAndClosingAllNodes.stories.tsx
+++ src/stories/examples/OpeningAndClosingAllNodes/OpeningAndClosingAllNodes.stories.tsx
@@ -1,9 +1,12 @@
 import { Meta } from "@storybook/react";
+import { expect } from "@storybook/jest";
+import { within, userEvent } from "@storybook/testing-library";
 import { pageFactory } from "~/stories/pageFactory";
 import * as argTypes from "~/stories/argTypes";
 import { Tree } from "~/Tree";
 import { TreeProps } from "~/types";
 import { FileProperties } from "~/stories/types";
+import { interactionsDisabled } from "~/stories/examples/interactionsDisabled";
 import { Template } from "./Template";
 import sampleData from "~/stories/assets/sample-default.json";
 import styles from "./OpeningAndClosingAllNodes.module.css";
@@ -36,3 +39,24 @@ OpeningAndClosingAllNodesStory.parameters = {
     }),
   },
 };
+
+if (!interactionsDisabled) {
+  OpeningAndClosingAllNodesStory.play = async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    expect(canvas.queryByText("File 1-2")).toBeNull();
+
+    const btnOpenAll = canvas.getByTestId("btn-open-all");
+    const btnCloseAll = canvas.getByTestId("btn-close-all");
+
+    userEvent.click(btnOpenAll);
+
+    expect(await canvas.findByText("File 1-2")).toBeInTheDocument();
+    expect(await canvas.findByText("File 2-1-1")).toBeInTheDocument();
+
+    userEvent.click(btnCloseAll);
+
+    expect(canvas.queryByText("File 1-2")).toBeNull();
+    expect(canvas.queryByText("File 2-1-1")).toBeNull();
+  };
+}
