15bdf3b4a1fdd2c53d3c129b95bb636df96a8e21
Minoru Okuyama <okuyama@analogic.jp>
2022-04-18 16:41:45 +0900

    refactor: remove __tests__ directory

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   roots: ["<rootDir>/src"],
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
-    "^.+\\.(css)$": "jest-transform-stub",
   },
   testRegex: "\\.(test|spec).(tsx?|jsx?)$",
   testEnvironment: "jsdom",
