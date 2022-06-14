a234e64aec44cc49367e5638438144f11c2fae56
Minoru Okuyama <okuyama@analogic.jp>
2022-06-14 23:44:09 +0900

    chore: modify jest configuration to support esm

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
-module.exports = {
+export default {
+  preset: "ts-jest/presets/js-with-babel",
   roots: ["<rootDir>/src"],
-  transform: {
-    "\\.tsx?$": "ts-jest",
-    "\\.jsx?$": "babel-jest",
-  },
   transformIgnorePatterns: [
     "/node_modules/(?!(@react-dnd|react-dnd-html5-backend|react-dnd-touch-backend)/)",
   ],
