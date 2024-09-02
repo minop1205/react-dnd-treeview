3bf8b5d7a20162c0f0ab49238e04f0de0a130241
Minoru Okuyama <okuyama@analogic.jp>
2024-09-02 23:26:02 +0900

    wip: update eslint config file

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
 import eslint from "@eslint/js";
 import importPlugin from "eslint-plugin-import";
-import prettierPlugin from "eslint-plugin-prettier";
+import prettierPlugin from "eslint-plugin-prettier/recommended";
+import hooksPlugin from "eslint-plugin-react-hooks";
 import reactPlugin from "eslint-plugin-react";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
+// configメソッドは flat config を拡張して config object に extends を独自追加している
 export default tseslint.config(
   {
     files: ["**/*.{js,jsx,ts,tsx}"],
@@ -14,15 +16,10 @@ export default tseslint.config(
     // which are ["**/node_modules/", ".git/"].
     ignores: ["dist", "storybook-static"],
   },
+
   {
     languageOptions: {
-      parser: tseslint.parser,
       parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: "module",
-        ecmaFeatures: {
-          jsx: true,
-        },
         project: "./tsconfig.json",
       },
       globals: {
@@ -32,72 +29,59 @@ export default tseslint.config(
     },
   },
 
-  {
-    plugins: {
-      "@typescript-eslint": tseslint.plugin,
-      //import: importPlugin,
-      react: reactPlugin,
-      prettier: prettierPlugin,
-    },
-  },
-
   // eslint.configs.recommended の実体は rules のみ入った ConfigurationObject
   eslint.configs.recommended,
 
   // redommendedTypeCheckedにはrecommendedも含まれるので、TypeCheckedの方だけでOK
-  //...tseslint.configs.recommended,
+  // recommendedTypeCheckedにはlanguageOptions, pluginsなどの設定も含まれる
   ...tseslint.configs.recommendedTypeChecked,
 
-  ...reactPlugin.configs.recommended,
+  // reactPlugin.configs.recommended はflat config形式で提供されている
+  // reactPlugin.configs.recommendedにはlanguageOptions.parserOptions.ecmaFeaturesも定義されている
+  reactPlugin.configs.recommended
 
-  {
-    rules: {
-      "import/order": [
-        "warn",
-        {
-          groups: [
-            "builtin",
-            "external",
-            "parent",
-            "sibling",
-            "index",
-            "object",
-            "type",
-          ],
-          pathGroups: [
-            {
-              pattern: "react",
-              group: "builtin",
-              position: "before",
-            },
-            {
-              pattern: "~/**",
-              group: "parent",
-              position: "before",
-            },
-          ],
-          pathGroupsExcludedImportTypes: ["type"],
-          alphabetize: {
-            order: "asc",
-          },
-        },
-      ],
-    },
-  }
+  // {
+  //   plugins: {
+  //     "react-hooks": hooksPlugin,
+  //   },
+  //   rules: hooksPlugin.configs.recommended.rules,
+  // },
 
-  //reactPlugin.configs.recommended
-  // importPlugin.configs.recommended
+  // {
+  //   rules: {
+  //     "import/order": [
+  //       "warn",
+  //       {
+  //         groups: [
+  //           "builtin",
+  //           "external",
+  //           "parent",
+  //           "sibling",
+  //           "index",
+  //           "object",
+  //           "type",
+  //         ],
+  //         pathGroups: [
+  //           {
+  //             pattern: "react",
+  //             group: "builtin",
+  //             position: "before",
+  //           },
+  //           {
+  //             pattern: "~/**",
+  //             group: "parent",
+  //             position: "before",
+  //           },
+  //         ],
+  //         pathGroupsExcludedImportTypes: ["type"],
+  //         alphabetize: {
+  //           order: "asc",
+  //         },
+  //       },
+  //     ],
+  //   },
+  // },
+
+  // // Prettierの設定は一番最後に置く
+  // prettierPlugin
 );
-
-// export default [
-//   {
-//     files: ["**/*.{js,jsx,ts,tsx}"],
-//   },
-//   {
-//     // This pattern is added after the default patterns,
-//     // which are ["**/node_modules/", ".git/"].
-//     ignores: ["dist", "storybook-static"],
-//   },
-//   eslint.configs.recommended,
-
-// ];
